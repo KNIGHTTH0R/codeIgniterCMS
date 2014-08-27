fd657a7648f767640af7e9f1880bfbbbe8dae7ec
victoravila <victor.janoras.avila@gmail.com>
2014-08-27 06:00:44 -0700

    Created new file 'core/MY_Model.php' and it will serve as the base model. Added protected variables that needs values once the base model is called. Also added empty functions 'get()', 'get_by()', 'save()', 'delete()' that will served as the generic CRUD.

---
+++ application/core/MY_Model.php
@@ -0,0 +1,20 @@
+<?php
+
+class MY_Model extends CI_Model {
+
+	protected $_table_name = '';
+	protected $_primary_key = 'id';
+	protected $_primary_filter = 'intval';
+	protected $_order_by = '';
+	protected $_rules = array();
+	protected $_timestamps =  FALSE;
+
+	public function __construct() {
+		parent::__construct();
+	}
+
+	public function get();
+	public function get_by();
+	public function save();
+	public function delete();
+}
