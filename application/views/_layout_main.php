f7543048a9742095d9e4393df75cb309d2352af3
victoravila <victor.janoras.avila@gmail.com>
2014-08-28 02:53:57 -0700

    Modified 'config/autoload.php' to load url helper. Modified '_layout_main.php'. Added html codes, added reference codes to css and js files. Modified controller 'dashboard.php' to load the main layout and modal layout  files. Added meta title to the Admin_Controller.php that will be the title for all the pages in the admin panel.

---
+++ application/views/_layout_main.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php echo $meta_title; ?></title>
+	<!-- Bootstrap -->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('public_html/css/bootstrap.min.css'); ?>">
+</head>
+<body>
+	<h1>Hello world</h1>
+	<script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
+	<script type="text/javascript" src="<?php echo base_url('public_html/js/bootstrap.min.js'); ?>"></script>
+</body>
+</html>
